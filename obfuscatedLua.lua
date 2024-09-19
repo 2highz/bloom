3b9b836385dbb412ba2a9a3ed9680c1cad2b08b6
2highz <2highclothingz@gmail.com>
2024-09-19 20:02:08 +1200

    Create obfuscatedLua.lua

---
+++ obfuscatedLua.lua
@@ -0,0 +1,68 @@
+--Obfuscated For Certian Reasons
+return function(z, b, J, V, s, r, I, h, t, W, p, o, P, Q, N, m, Y, O, i, B, n, G, l, q, u, a, A, v, F, E, Z, k)
+      b = {}
+      local x = 117
+      
+      while true do
+          if x < 121.0 and x > 111.0 then
+              if not b[13888] then
+                  x = -2942162959 + a.z(a.u(a.D(k[7], 22))) - k[6]
+                  b[0x3640] = x
+              else
+                  x = b[0x3640]
+              end
+          elseif x < 117.0 and x > 80.0 then
+              if not b[0x7972] then
+                  b[2601] = 43 + a.u(a.t(x) - k[0x08] > k[0x08] and k[0x03] or k[0x007])
+                  x = -545306068 + (a.r(a.r(x) - k[0x01], k[0x01]) > k[0x03] and k[4] or k[3])
+                  b[0x7972] = x
+              else
+                  x = b[0x7972]
+              end
+          elseif x > 117.0 then
+              break
+          else
+              if x < 80.0 then
+                  if not b[0x4D74] then
+                      x = b[19828]
+                  else
+                      x = -3770125625 + a.z((k[1] > k[0x05] and x or b[13888]) + b[2601] < b[0x3640] and k[0x009] or k[0x07])
+                      b[0x4D74] = x
+                  end
+              elseif x > 2.0 and x < 111.0 then
+                  if not b[13471] then
+                      x = b[0x349f]
+                  else
+                      x = -0x397DA9DF + a.H(a.A(k[4] == k[9] and k[7] or k[0x04]) + k[0x09], 11)
+                      b[0x349f] = x
+                  end
+              end
+          end
+      end
+      
+      local _, c, T
+      x = 0x33
+      
+      repeat
+          if x > 51.0 then
+              break
+          else
+              if x < 118.0 then
+                  _ = a.K
+                  c = {}
+                  T = 1.0
+                  if not b[6723] then
+                      x = 77 + a.o(a.o(a.A(b[0x7972], k[3])) - k[0x05], b[19828], b[0x4D74])
+                      b[0x1a43] = x
+                  else
+                      x = b[6723]
+                  end
+              end
+          end
+      until false
+      
+      local R, d
+      x = 7
+      
+      repeat
+          if not
